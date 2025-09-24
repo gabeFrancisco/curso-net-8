4e2ba4f72528d86a6519b07454532a752786b282
gabeFrancisco <gabegit2@gmail.com>
2025-09-24 14:26:06 -0300

    adicionado relacionamento entre entidades Categoria e Produto

--- ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/Models/Categoria.cs
@@ -1,6 +1,16 @@
+using System.Collections.ObjectModel;
+
 public class Categoria
 {
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImageUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
+
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
 }
