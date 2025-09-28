26c0c18b8aa91047c75116996f1d4fe49e0d91c1
gabeFrancisco <gabegit2@gmail.com>
2025-09-28 00:18:35 -0300

    criado metodo delete em ProdutosController

--- ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ApiCatalogo.Context;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalogo.Controllers
 {
@@ -20,7 +21,7 @@ namespace ApiCatalogo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            var produtos = _context.Produtos.ToList();
+            var produtos = _context.Produtos!.ToList();
             if (produtos is null)
             {
                 return NotFound();
@@ -28,10 +29,10 @@ namespace ApiCatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("{id}", Name ="ObterProduto")]
+        [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
-            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            var produto = _context.Produtos!.FirstOrDefault(p => p.ProdutoId == id);
             if (produto is null)
             {
                 return NotFound();
@@ -47,11 +48,41 @@ namespace ApiCatalogo.Controllers
             {
                 return BadRequest();
             }
-            
-            _context.Produtos.Add(produto);
+
+            _context.Produtos!.Add(produto);
             _context.SaveChanges();
 
             return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId });
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos!.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            _context.Produtos!.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
     }
 }
